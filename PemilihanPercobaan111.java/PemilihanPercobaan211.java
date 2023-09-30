14d7be58e8615db7bb6d4eadebc96dba03ef22e0
JohnatanDavid <144687283+JohnatanDavid@users.noreply.github.com>
2023-09-30 14:26:21 +0700

    Update PemilihanPercobaan211.java

--- PemilihanPercobaan111.java/PemilihanPercobaan211.java
+++ PemilihanPercobaan111.java/PemilihanPercobaan211.java
@@ -13,5 +13,33 @@ public class PemilihanPercobaan211 {
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) +(tugas * 0.2F);
         String message = total < 65 ? "Remidi" : "Tidak remidi";
         System.out.println("Nilai akhir = " + total + " sehingga " + message);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan nilai akhir: ");
+        int nilai = input.nextInt();
+        String nilaiHuruf = "";
+
+        if (nilai >= 80) {
+            nilaiHuruf = "A";
+        } 
+        else if (nilai >= 73) {
+            nilaiHuruf = "B+";            
+        } 
+        else if (nilai >= 65) {
+            nilaiHuruf = "B";            
+        } 
+        else if (nilai >= 60){
+             nilaiHuruf = "C+";
+        }
+        else if (nilai >= 50) {
+            nilaiHuruf = "C";
+        } 
+        else if (nilai >= 39) {
+            nilaiHuruf = "D";           
+        } 
+        else {
+            nilaiHuruf = "E";            
+        }
+        System.out.printf("Nilai huruf = %s\n", nilaiHuruf);
+    }
     }
 }
